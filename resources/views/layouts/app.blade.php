3cb0a4a46497caa38471e51c3e6820737d23b329
bayezsyka <farrosy6@gmail.com>
2025-12-23 16:56:18 +0700

    build ulang

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="api-base-url" content="{{ env('FRONTEND_API_BASE_URL', 'http://127.0.0.1:8000/api') }}">
+    <meta name="api-base-url" content="{{ env('FRONTEND_API_BASE_URL', '/api') }}">
     
     <!-- PWA Meta Tags -->
     <meta name="theme-color" content="#008362">
